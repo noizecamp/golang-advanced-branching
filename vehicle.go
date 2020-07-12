1783d75b0a5450e02f3a095544e98f0190637ecd
Kyaw Phyu Aung <kyaw.phyu.aung@autodesk.com>
2020-07-12 12:26:06 +0800

    Activated init method

--- vehicle.go
+++ vehicle.go
@@ -2,11 +2,24 @@ package main
 
 // Values array for the feedback.json file
 type Values struct {
+	Models []Model `json:"values"`
 }
 
 // Model array for the feedback.json file
 type Model struct {
+	Name     string   `json:"model"`
+	Feedback []string `json:"feedback"`
 }
+
+type feedbackResult struct {
+	feedbackTotal    int
+	feedbackPositive int
+	feedbackNegative int
+	feedbackNeutral  int
+}
+
+var vehicleResult map[string]feedbackResult
+var inventory vehicle
 
 type rating float32
 
@@ -40,23 +53,22 @@ const (
 )
 
 func init() {
-
-	/*
-		inventory = []vehicle{
-			bike{"FTR 1200", "Indian"},
-			bike{"Iron 1200", "Harley"},
-			car{"Sonata", "Hyundai", "Sedan"},
-			car{"SantaFe", "Hyundai", "SUV"},
-			car{"Civic", "Honda", "Hatchback"},
-			car{"A5", "Audi", "Coupe"},
-			car{"Mazda6", "Mazda", "Sedan"},
-			car{"CRV", "Honda", "SUV"},
-			car{"Camry", "Toyota", "Sedan"},
-			truck{"F-150", "Ford", "Truck"},
-			truck{"RAM1500", "Dodge", "Truck"}}
-	*/
-	//	vehicleResult = make(map[string]feedbackResult)
-
+	
+	inventory = []vehicle{
+		bike{"FTR 1200", "Indian"},
+		bike{"Iron 1200", "Harley"},
+		car{"Sonata", "Hyundai", "Sedan"},
+		car{"SantaFe", "Hyundai", "SUV"},
+		car{"Civic", "Honda", "Hatchback"},
+		car{"A5", "Audi", "Coupe"},
+		car{"Mazda6", "Mazda", "Sedan"},
+		car{"CRV", "Honda", "SUV"},
+		car{"Camry", "Toyota", "Sedan"},
+		truck{"F-150", "Ford", "Truck"},
+		truck{"RAM1500", "Dodge", "Truck"}
+	}
+	
+	vehicleResult = make(map[string]feedbackResult)
 }
 
 func main() {
